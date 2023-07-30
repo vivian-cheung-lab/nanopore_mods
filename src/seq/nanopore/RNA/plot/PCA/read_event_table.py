4d80255fb2ccaff6492b889f98b219c017586a16
Josh Burdick <jtburd@umich.edu>
2023-07-30 12:36:35 -0400

    Sketch getting "continuous" regions

--- src/seq/nanopore/RNA/plot/PCA/read_event_table.py
+++ src/seq/nanopore/RNA/plot/PCA/read_event_table.py
@@ -20,20 +20,51 @@ def read_event_matrices():
     """Reads in the events."""
     print('[reading events]')
     matrices = dict()
-    for r in sample_table[:10].itertuples():
+    for r in sample_table[:3].itertuples():
         print('.', end='')
         event_data = np.load(f'{matrix_dir}/{r.FASTQ_file_base_name}.npz')
         m = event_data['event_stats']
         matrices[r.name_and_concentration] = m
         sys.stdout.flush()
     print()
+    return matrices
+
+
+def get_count_of_nonmissing_ranges(x):
+    # we just use whether current is present
+    # (as time is present at same sites)
+    current_present = ~ np.isnan(x[:,0,:])
+    # get cumulative sum, forward
+
+
+    # ... and reversed
+
+
+    # np.outer(
+    pdb.set_trace()
+
+
+def get_means_for_missing_values():
+    """Gets means at each position to fill in missing values.
+    
+    This will be current and time at each position, from the
+    unmodified samples.
+    """
+    unmodified_samples = [matrices[s]
+            for s in matrices.keys()
+            where s.endswith(' none')]
+    pdb.set_trace()
 
 
 
-def get_count_of_nonmissing_ranges(event_stats):
-    pass    
 
 
-m = read_event_matrices()
+matrices = read_event_matrices()
+
+present_count = [get_count_of_nonmissing_ranges(m) for m in matrices.values()]
+
+
+pdb.set_trace()
+
 
 
