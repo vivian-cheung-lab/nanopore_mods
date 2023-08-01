8aea3eeee67f2e53c2e8dad4d5cdd3f71dbd1fe0
Josh Burdick <jtburd@umich.edu>
2023-07-31 22:54:47 -0400

    Write PCA (with rows, or columns, standardized)

--- src/seq/nanopore/RNA/plot/PCA/PCA.py
+++ src/seq/nanopore/RNA/plot/PCA/PCA.py
@@ -10,21 +10,33 @@ import sklearn.preprocessing
 
 # read in table
 event_table = pandas.read_csv('read_event_table.csv', index_col=0)
+event_table.reset_index(drop=True, inplace=True)
+
 # convert to numpy
 X = event_table.iloc[:,1:].to_numpy()
 
 def standardize(x):
-    """Standardizes a data matrix."""
+    """Standardizes the columns of a data matrix."""
     scaler = sklearn.preprocessing.StandardScaler()
     scaler.fit(x)
     return scaler.transform(x)
 
-X = standardize(X)
-pca = sklearn.decomposition.PCA(n_components=3)
-Y = pca.fit_transform(X)
+def write_PCA(X, output_filename):
+    """Computes and writes out a PCA."""
+    pca = sklearn.decomposition.PCA(n_components=3)
+    Y = pca.fit_transform(X)
+    Y = pandas.DataFrame(Y, columns = ['PC1', 'PC2', 'PC3'])
+    Y = np.round(Y, 5)
+    r = pandas.concat([event_table['sample name'], pandas.DataFrame(Y)], axis=1)
+    r.to_csv(output_filename)
 
-
-pdb.set_trace()
-
-# ??? or standardize rows?
+# write out with columns standardized (as before)
+write_PCA(standardize(X), 'PCA_columns_standardized.csv')
+# ... and with rows standardized (treating current and time separately)
+n = X.shape[1] // 2
+X1 = np.concatenate([
+    standardize(X[:,:n].T).T,
+    standardize(X[:,n:(2*n)].T).T,
+    ], axis=1)
+write_PCA(X1, 'PCA_rows_standardized.csv')
 
