5be9e0868df026944c4be1708571ab127b99553b
Josh Burdick <jtburd@umich.edu>
2023-08-01 15:20:56 -0400

    Color modifications

--- src/seq/nanopore/RNA/plot/PCA/plot/current_and_time_PCA_2.py
+++ src/seq/nanopore/RNA/plot/PCA/plot/current_and_time_PCA_2.py
@@ -5,13 +5,20 @@ import os
 import pdb
 
 import matplotlib.pyplot as plt
+import matplotlib.colors
+import numpy as np
 import pandas
+
+from scipy.linalg import sqrtm
 
 # read in PCA from Prism
 # (this may change to using sklearn)
 # pca = pandas.read_table('../PCA of Current and dwell time 1.txt')
 # using sklearn
 pca = pandas.read_csv('../PCA_columns_standardized.csv')
+# add some annotations
+pca['mod_name'] = [s.split(' ')[0] for s in pca['sample name']]
+pca['concentration'] = [s.split(' ')[1] for s in pca['sample name']]
 
 # where to write output
 output_dir = 'current_and_time_PCA_2'
@@ -24,41 +31,98 @@ concentration_levels = {
         '1:10': 2,
         '1:5': 3,
         '1:2': 4,
-#        '100%': 5
+        '100%': 5
         }
-
-
+mod_names = list(set(pca['mod_name']))
+mod_names.sort()
+# set hues for the modifications
+hue = dict(zip(mod_names, np.linspace(0, 1, len(mod_names), endpoint=False)))
+# get color scheme for these
+sample_color = dict()
+for i in pca[['sample name', 'mod_name', 'concentration']].drop_duplicates().iterrows():
+    sample_color[i[1]['sample name']] = matplotlib.colors.hsv_to_rgb((
+            hue[i[1]['mod_name']],
+            0.75,
+            concentration_levels[i[1]['concentration']] / 5))
 
 # get bounds (so that all the plots can be on the same scale)
 pca_bounds = pca.loc[:,['PC1','PC2','PC3']].quantile([0,1])
 
-mod_names = list(set([s.split(' ')[0] for s in set(pca['sample name'])]))
-mod_names.sort()
 
-def plot_PCA_for_mod(mod_name, hue):
+
+
+def std_ellipse(sigma, num_points=1000):
+    """Gets points corresponding to a 1-sd ellipse.
+
+    Adapted from (Randolf Scholz' comment)[https://gist.github.com/CarstenSchelp/b992645537660bda692f218b562d0712?permalink_comment_id=3465086#gistcomment-3465086]
+    sigma: the covariance matrix
+    num_points: how many points in a circle to use
+    Returns: a numpy array with shape (2, num_points), of
+        the points on the ellipse.
+    """
+    T = np.linspace(0, 2*np.pi, num=num_points)
+    circle = radius * np.vstack([np.cos(T), np.sin(T)])
+    x, y = sqrtm(Sigma) @ circle
+#    plt.plot(x, y, '-r', linewidth=5)
+
+
+def plot_center_and_conf_ellipse(mu, sigma, scale=2.44, color='black', lw=2):
+    """
+
+    mu, sigma: mean and covariance
+    scale: the scale of the ellipse (relative to s.d. along each major axis)
+        According to the link below, setting the half-axis scale to
+        2.44 = sqrt(5.991) (approximately) will give an ellipse
+        covering 95% of the points.
+        https://www1.udel.edu/biology/rosewc/kaap686/reserve/cop/center%20of%20position%20conf95.pdf
+    color: color to use
+    r: mean of radius
+    lw: width of line to use
+    Side effects: plots the mean (as a point) and the (scaled) s.d. ellipse
+    """
+
+
+    plt.scatter(mu, c=color, r=lw)
+    plt.plot(mu + scale * std_ellipse(sigma), c=color, lw=lw)
+
+
+def plot_PCA_for_mod(mod_name):
     """Plots PCA of all the concentrations of one modification.
 
-    mod_name: the name of the modification
-    hue: the hue to use for plotting this modification
+    mod_name: the name of the modification to plot
     Side effects: plots PCA for that modification
     """
+    hue = 2/3   # FIXME set this in advance
     # using the first two components seems to work reasonably well
     components = ['PC1', 'PC2']
     os.makedirs(output_dir, exist_ok=True)
-    x = pca.loc[:,['sample name',components[0],components[1]]]
-    x = x[ x['sample name'].str.startswith(mod_name) ]
-    x['concentration'] = x['sample name'].str.replace(mod_name + ' ', '')
     # set up plotting
     plt.figure(figsize=(9,6))
     for concentration in concentration_levels.keys():
-        x1 = x[x.concentration==concentration]
+        x1 = pca[(pca.mod_name==mod_name) & (pca.concentration==concentration)]
+        if x1.empty:
+            continue
+        # pdb.set_trace()
+        sample_name = f'{mod_name} {concentration}'
         plt.scatter(x1[components[0]], x1[components[1]],
-                label=concentration, alpha=0.4)
+                label=sample_name,
+                color=sample_color[sample_name],
+                alpha=0.8)
+        # FIXME plot other ways
+    # label axes
     plt.xlabel(components[0])
     plt.ylabel(components[1])
+    # set bounds (to be consistent across plots)
+    xlim=pca_bounds[components[0]]
+    plt.xlim(xlim[0], xlim[1])
+    ylim=pca_bounds[components[1]]
+    plt.ylim(ylim[0], ylim[1])
+    # FIXME add legend
+    plt.legend()
+
     plt.savefig(f'{output_dir}/{mod_name}.png')
 
 for mod in mod_names:
     print(mod)
-    plot_PCA_for_mod(mod, 2/3)
+    plot_PCA_for_mod(mod)
 
