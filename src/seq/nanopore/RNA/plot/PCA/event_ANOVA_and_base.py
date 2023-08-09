ab6c8eeb426c2a4d23e18fb710e0ab98c1fb3551
Josh Burdick <jtburd@umich.edu>
2023-08-09 15:01:18 -0400

    Add base composition

--- src/seq/nanopore/RNA/plot/PCA/event_ANOVA_and_base.py
+++ src/seq/nanopore/RNA/plot/PCA/event_ANOVA_and_base.py
@@ -3,6 +3,7 @@
 
 import pdb
 
+import numpy as np
 import pandas
 
 import pysam
@@ -23,5 +24,28 @@ ANOVA_table = ANOVA_table.iloc[:,2:]
 # note that the base index in the table is 1-based
 ANOVA_table['base'] = [clone_seq[i-1] for i in ANOVA_table.index]
 
-ANOVA_table.to_xlsx('event_ANOVA_and_base.xlsx')
+def base_composition_at_cutoff(proportion_to_include):
+    """Gets base composition of the sites with the top % of F-statistics."""
+    def above_cutoff(x):
+        cutoff = np.quantile(x, 1-proportion_to_include)
+        return x >= cutoff
+    bases = ANOVA_table[
+            above_cutoff(ANOVA_table.statistic.current) |
+            above_cutoff(ANOVA_table.statistic.time) ].base
+    bases = bases.str.replace('T', 'U')
+    base_composition = bases.to_frame().groupby('base').size().to_frame()
+    base_composition['cutoff'] = proportion_to_include
+    return base_composition
+
+base_composition = pandas.concat([base_composition_at_cutoff(cutoff)
+    for cutoff in [0.01, 0.05, 0.1, 0.25, 0.5]])
+
+base_composition.reset_index(inplace=True)
+base_composition = base_composition.pivot(index=['cutoff'], columns='base')
+base_composition.columns = base_composition.columns.droplevel(0)
+# pdb.set_trace()
+
+with pandas.ExcelWriter('event ANOVA and base.xlsx', engine='xlsxwriter') as writer:
+      ANOVA_table.to_excel(writer, sheet_name='ANOVA and central base')
+      base_composition.to_excel(writer, sheet_name='Base composition', index=False)
 
