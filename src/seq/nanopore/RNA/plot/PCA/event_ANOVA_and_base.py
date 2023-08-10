f3d0417e7c5033ca5d514b4c90518c4c145aaf0b
Josh Burdick <jtburd@umich.edu>
2023-08-10 14:53:15 -0400

    Write just events-above-cutoff
    Including bases in those 5-mers.

--- src/seq/nanopore/RNA/plot/PCA/event_ANOVA_and_base.py
+++ src/seq/nanopore/RNA/plot/PCA/event_ANOVA_and_base.py
@@ -19,34 +19,24 @@ ANOVA_stats['measurement'] = [s[0]
 ANOVA_stats['base_number'] = [int(s[1])
         for s in ANOVA_stats.iloc[:,0].str.split()]
 
-ANOVA_table = ANOVA_stats.pivot(index='base_number', columns='measurement')
-ANOVA_table = ANOVA_table.iloc[:,2:]
+# fraction of most-significant columns to include
+quantile_cutoff = 0.05
+
+def above_cutoff(x):
+    """Gets only the rows with 'statistic' column above a cutoff."""
+    cutoff = np.quantile(x.statistic, 1-quantile_cutoff)
+    return x[ x.statistic >= cutoff ]
+
+ANOVA_table = pandas.concat([
+    above_cutoff(ANOVA_stats[ ANOVA_stats.measurement=='current' ]),
+    above_cutoff(ANOVA_stats[ ANOVA_stats.measurement=='time' ]) ])
 # note that the base index in the table is 1-based
-ANOVA_table['base'] = [clone_seq[(i-3):(i+2)].replace('T', 'U')
-        for i in ANOVA_table.index]
+ANOVA_table['bases'] = [clone_seq[(i-3):(i+2)].replace('T', 'U')
+        for i in ANOVA_table.base_number]
 
-def base_composition_at_cutoff(proportion_to_include):
-    """Gets base composition of the sites with the top % of F-statistics."""
-    def above_cutoff(x):
-        cutoff = np.quantile(x, 1-proportion_to_include)
-        return x >= cutoff
-    bases = ANOVA_table[
-            above_cutoff(ANOVA_table.statistic.current) |
-            above_cutoff(ANOVA_table.statistic.time) ].base.str[2]
-    base_composition = bases.to_frame().groupby('base').size().to_frame()
-    base_composition['cutoff'] = proportion_to_include
-    return base_composition
+print('base composition:')
+print(ANOVA_table.bases.str[2].to_frame().groupby('bases').size())
 
-base_composition = pandas.concat([base_composition_at_cutoff(cutoff)
-    for cutoff in [0.01, 0.05, 0.1, 0.25, 0.5]])
-# pdb.set_trace()
-
-base_composition.reset_index(inplace=True)
-base_composition = base_composition.pivot(index=['cutoff'], columns='base')
-base_composition.columns = base_composition.columns.droplevel(0)
-# pdb.set_trace()
-
-with pandas.ExcelWriter('event ANOVA and base.xlsx', engine='xlsxwriter') as writer:
-    ANOVA_table.to_excel(writer, sheet_name='ANOVA and central base')
-    base_composition.to_excel(writer, sheet_name='Base composition')
+with pandas.ExcelWriter('event ANOVA.xlsx', engine='xlsxwriter') as writer:
+    ANOVA_table.to_excel(writer, sheet_name='Column f-statistics')
 
